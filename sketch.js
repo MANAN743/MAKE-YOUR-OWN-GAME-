b80b48f29afcb39323314de39ca10229cc75829b
MANAN743 <62739495+MANAN743@users.noreply.github.com>
2020-11-13 17:43:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(displayWidth/2,displayHeight);
+	
+
+}
+
+
+function draw() {
+  background(0);
+  
+  spawnTiles()
+  drawSprites();
+}
+function spawnTiles(){
+	var tiles=createSprite(50,30)
+}
+
+
