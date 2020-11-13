4b584234c897591cf23ec017d9087cafa502f633
MANAN743 <malhotrasamta@gmail.com>
2020-11-13 18:30:02 +0530

    commit2

--- sketch.js
+++ sketch.js
@@ -1,27 +1,47 @@
-
+var TileGroup;
 function preload()
 {
 	
 }
 
 function setup() {
-	createCanvas(displayWidth/2,displayHeight);
-	
+	createCanvas(windowWidth/2,windowHeight);
+	TileGroup=new Group();
 
 }
 
 
 function draw() {
   background(0);
+  for(var i=0;i<TileGroup.length;i++){
+    if(mousePressedOver(TileGroup.get(0))){
+      TileGroup.get(0).destroy();
+    }
+  }
+  for(var i=0;i<TileGroup.length;i++){
+if(TileGroup.get(0).y>displayHeight){
+  text("GameOver",width/2,height/2)
+}
+  }
   
   spawnTiles()
   drawSprites();
 }
 function spawnTiles(){
-  
-  var tiles=createSprite(50,0,40,50)
-  tiles.velocityY=2
-
+  if (frameCount%40===0){
+  var tiles=createSprite(50,0,100,150)
+  tiles.velocityY=4+frameCount/100
+  var num=Math.round(random(1,3))
+  switch(num){
+    case 1:tiles.x=width/4;
+    break;
+    case 2:tiles.x=width/2;
+    break;
+    case 3:tiles.x=3*width/4;
+    break;
+  }
+  TileGroup.add(tiles);
+  }
 }
 
 
