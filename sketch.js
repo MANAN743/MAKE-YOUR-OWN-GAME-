89598e86719af0f8d4807b81d548790ea8fd228a
MANAN743 <malhotrasamta@gmail.com>
2020-11-15 19:45:20 +0530

    Commit 4

--- sketch.js
+++ sketch.js
@@ -1,43 +1,55 @@
 var TileGroup;
 var score=0
+var NaachMeriRanisound;
+var gameState="Play"
 function preload()
 {
-	backgroundIMG=loadImage("background.png")
+  backgroundIMG=loadImage("background.png")
+ NaachMeriRanisound=loadSound("Naach Meri Rani Song - Guru Randhawa 128 Kbps.mp3")
 }
-
 function setup() {
 	createCanvas(windowWidth/2,windowHeight);
-	TileGroup=new Group();
-
+  TileGroup=new Group();
+   NaachMeriRanisound.play(); 
 }
-
 
 function draw() {
   background(backgroundIMG);
-  for(var i=0;i<TileGroup.length;i++){
-    if(mousePressedOver(TileGroup.get(0))){
-      TileGroup.get(0).destroy();
-      score++
-    }
-  }
-  for(var i=0;i<TileGroup.length;i++){
-if(TileGroup.get(0).y>displayHeight){
-  textSize(40)
-  fill("red")
-  strokeWeight(2);
-stroke(15);
-  text("GameOver",width/2,height/2)
-}
-  }
   textSize(30)
   fill("white")
   strokeWeight(2);
 stroke(15);
   text("Score: "+score,20,20)
+  if(gameState==="Play"){
+  for(var i=0;i<TileGroup.length;i++){
+    if(mousePressedOver(TileGroup.get(0))){
+      TileGroup.get(0).destroy();
+      score++
+      
+  //NaachMeriRanisound.play();
+    }
+  }
+  for(var i=0;i<TileGroup.length;i++){
+if(TileGroup.get(0).y>displayHeight){
+  gameState="End"
+  
+}
+  }
+
+  
   
   spawnTiles()
+}
+if(gameState==="End"){
+  textSize(40)
+  fill("red")
+  strokeWeight(2);
+stroke(15);
+  text("GameOver",width/2,height/2)
+  NaachMeriRanisound.pause();
+}
   drawSprites();
-  
+
 }
 function spawnTiles(){
   if (frameCount%40===0){
